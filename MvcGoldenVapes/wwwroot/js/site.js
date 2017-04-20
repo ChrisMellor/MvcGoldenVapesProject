14d648a02dcb8653bdcfc48e87ee2a9ccd0b1a2f
Christopher Stuart Mellor <st20065022@outlook.cardiffmet.ac.uk>
2017-04-20 19:11:18 +0100

    Fixed Payment to load

--- MvcGoldenVapes/wwwroot/js/site.js
+++ MvcGoldenVapes/wwwroot/js/site.js
@@ -1,17 +1,14 @@
-﻿
-
-
-function validateCard(inputText)
+﻿function validateCard(inputText)
 {
     var creditcard = (^(?:4[0 - 9]{12}(?:[0 - 9]{3}) ?|5[1 - 5][0 - 9]{14 }| 6(?:011 | 5[0 - 9][0 - 9])[0 - 9]{12 }| 3[47][0 - 9]{13 }| 3(?:0[0 - 5] | [68][0 - 9])[0 - 9]{11 }|(?:2131 | 1800 | 35\d{3 }) \d{11 })$  )
      if (inputText.value.match(creditcard))
-        {
-         document.contactPage.email.focus();
+     {
+         document.inputText.focus();
          return true;
         }
      else {
-          alert("You have entered an invalid email address!");
-          document.contactPage.email.focus();
+          alert("You have entered an invalid Bank card number!");
+          document.inputText.focus();
           return false;
          }
 }
