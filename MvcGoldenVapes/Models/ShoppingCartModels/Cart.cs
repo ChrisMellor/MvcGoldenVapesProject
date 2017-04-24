ea05818b0bff1489244afd784c5df562663c2751
Christopher Stuart Mellor <st20065022@outlook.cardiffmet.ac.uk>
2017-04-24 18:56:49 +0100

    Renamed files to allow navigation as intended

--- MvcGoldenVapes/Models/ShoppingCartModels/Cart.cs
+++ MvcGoldenVapes/Models/ShoppingCartModels/Cart.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace MvcGoldenVapes.Models
 {
@@ -12,7 +13,9 @@ namespace MvcGoldenVapes.Models
     public class Cart
     { 
         [Key]
+        
         public int VapeID { get; set; }
+        [DisplayName  ("Vape Name")]
         public string VapeName { get; set; }
         public double VapePrice { get; set; }
         public string Quantity { get; set; }
