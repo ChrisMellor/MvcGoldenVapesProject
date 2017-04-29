3d003e5ccc13b583b5fbfc676e1693afbe0acd07
Christopher Stuart Mellor <st20065022@outlook.cardiffmet.ac.uk>
2017-04-29 17:07:31 +0100

    Somewhat finished Code

--- MvcGoldenVapes/Models/ShoppingCartModels/Cart.cs
+++ MvcGoldenVapes/Models/ShoppingCartModels/Cart.cs
@@ -16,14 +16,31 @@ namespace MvcGoldenVapes.Models
         [Key]
         [DisplayName("Vape ID")]
         public int VapeID { get; set; }
+
+        [Required]
         [DisplayName  ("Vape Name")]
+        [DataType(DataType.Text)]
+        [StringLength(35)]
+        [RegularExpression(@"^([a - zA - Z] +)$", ErrorMessage = "Invalid Name for Vape Product")]
         public string VapeName { get; set; }
+
+        [Required]
         [DisplayName("Vape Price")]
         [DataType(DataType.Currency)]
+        [Range(0, 999.99)]
+        [RegularExpression(@"^[1-9]\d*(\.\d+)?$", ErrorMessage = "Invalid Price")]
         public double VapePrice { get; set; }
+
+        [Required]
+        [Range(0, 25)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Invalid Qauntity")]
         public int Quantity { get; set; }
+
         [DisplayName("Sub Total")]
         [DataType(DataType.Currency)]
+        [RegularExpression(@"^[1-9]\d*(\.\d+)?$", ErrorMessage = "Invalid Sub Total")]
+        [Required]
+        [Range(0, 999.99)]
         public double SubTotal { get; set; }
     }
 
--- MvcGoldenVapes/Models/ShoppingCartModels/vapeProducts.cs
+++ MvcGoldenVapes/Models/ShoppingCartModels/vapeProducts.cs
@@ -14,18 +14,39 @@ namespace MvcGoldenVapes.Models
     public class VapeProducts
     {
         [Key]
+
+        [Required]
         [DisplayName("Vape ID")]
         public int VapeID { get; set; }
+
+        [Required]
         [DisplayName("Vape Brand")]
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^([a - zA - Z] +)$", ErrorMessage = "Invalid Name for Vape Brand")]
         public string VapeBrand { get; set; }
+
+        [Required]
         [DisplayName("Vape Name")]
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^([a - zA - Z] +)$", ErrorMessage = "Invalid Name for Vape Product")]
         public string VapeName { get; set; }
+
+        [Required]
         [DisplayName("Vape Stock")]
+        [Range(0, 25)]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Invalid Qauntity")]
         public int VapeStock { get; set; }
+
+        [Required]
         [DisplayName("Vape Price")]
         [DataType(DataType.Currency)]
+        [Range(0, 999.99)]
+        [RegularExpression(@"^[1-9]\d*(\.\d+)?$", ErrorMessage = "Invalid Total")]
         public double VapePrice { get; set; }
+
+        [Required]
         [DisplayName("Vape Size")]
+        [DataType(DataType.Text)]
         public string VapeSize { get; set; }
     }
 }
